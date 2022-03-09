b97895363c9b287ec58b7132bd173397c1916fc4
Rajkumar G <mightycoolhotraj@gmail.com>
2022-03-09 10:08:33 +0530

    hosting related changes

--- angular-mfe-example/projects/host/src/environments/environment.prod.ts
+++ angular-mfe-example/projects/host/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  mfe: {
+    mfe1Url: "http://localhost:4201/mfe1remoteEntry.js",
+    mfe2Url: "http://localhost:4202/mfe2remoteEntry.js"
+  }
 };
